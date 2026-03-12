c2a037cf6db62d514beb79e6a2aa0045c6e742be
lenhhoixung3 <luuductai.bds@gmail.com>
2026-03-13 02:02:58 +0700

    Update app.js

--- css/js/app.js
+++ css/js/app.js
@@ -1,4 +1,13 @@
 // Main Application Logic
+
+function doOptions() {
+  return ContentService.createTextOutput("")
+    .setMimeType(ContentService.MimeType.JSON)
+    .setHeader("Access-Control-Allow-Origin", "*")
+    .setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    .setHeader("Access-Control-Allow-Headers", "Content-Type");
+}
+
 class InventoryApp {
     constructor() {
         this.scanner = null;
